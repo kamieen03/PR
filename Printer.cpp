fb3d6ca360df087682313336fb2ae059ae96c978
bonczol <filip.bonczyk@student.put.poznan.pl>
2019-06-03 12:45:38 +0200

    Color fix ";" added

--- Printer.cpp
+++ Printer.cpp
@@ -1,4 +1,4 @@
 #include "Printer.h"
 static void print(std::string msg, int nr){
-    std::cout << "\033[03" << nr <<"mProcess " << nr << ": " << msg << std::endl;
+    std::cout << "\033[0;3" << nr <<"m Process " << nr << ": " << msg << std::endl;
 }
--- Printer.h
+++ Printer.h
@@ -4,15 +4,7 @@
 #include <iostream>
 
 class Printer{
-    //Black        "\033[0;30m"
-    //Red          "\033[0;31m"
-    //Green        "\033[0;32m"
-    //Brown/Orange "\033[0;33m"
-    //Blue         "\033[0;34m"
-    //Purple       "\033[0;35m"
-    //Cyan         "\033[0;36m"
-    //Light Gray   "\033[0;37m"
-
+public:
     static void print(std::string msg, int nr);
 };
 
