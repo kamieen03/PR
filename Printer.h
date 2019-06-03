422303b8fb668b265abb3222b4423f7b5ffd7aae
bonczol <filip.bonczyk@student.put.poznan.pl>
2019-06-03 12:39:16 +0200

    prints in receiver added

--- Printer.h
+++ Printer.h
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <list>
 #include "types.h"
 
 class Printer{
 public:
-    static void print(std::string msg, int nr);
+    static void print(const std::list<std::string> &args, int nr);
     static std::string state2str(enum State);
     static std::string weapon2str(enum weaponType);
 };
